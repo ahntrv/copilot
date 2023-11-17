7ffccaea09dd760046ad1e3bcda466eb7bf79f1b
ahntrv <151053073+ahntrv@users.noreply.github.com>
2023-11-16 22:39:02 -0600

    Update script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleDarkMode() {
+    const body = document.body;
+    const currentTheme = body.classList.contains('dark-theme') ? 'light-theme' : 'dark-theme';
+    body.classList.toggle('dark-theme');
+
+    const toggleIcon = document.getElementById('toggle-icon');
+    toggleIcon.textContent = currentTheme === 'dark-theme' ? '☀️' : '🌙';
+}
