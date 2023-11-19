352d30a0f3ee503b66aaa33782831d6967140f38
ahntrv <151053073+ahntrv@users.noreply.github.com>
2023-11-19 14:23:32 -0600

    Create mendable-script.js

---
+++ mendable-script.js
@@ -0,0 +1,11 @@
+// mendable-script.js
+
+// Mendable widget configuration
+Mendable.initialize({
+    anon_key: 'e5626e3e-7809-4aba-8a67-104b4446dec1',
+    type: "searchBar",
+    elementId: "mendable-component", // required
+    placeholder: "Ask Optimus", // add this line
+    dialogPlaceholder: "Ask me anything" // add this line
+    // all the other props for the component type
+});
