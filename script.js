34d2335e9acc96f1e29c1b5c66b4e35abe5e2df5
ahntrv <151053073+ahntrv@users.noreply.github.com>
2023-11-18 22:54:30 -0600

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,43 @@ var tradingViewConfig = {
     ],
     "chartOnly": false,
     "width": "100%", /* Set the widget width to 100% */
-    // ... (rest of the configuration)
+    "height": "100%",
+  "locale": "en",
+  "colorTheme": "dark",
+  "autosize": true,
+  "showVolume": false,
+  "showMA": false,
+  "hideDateRanges": false,
+  "hideMarketStatus": false,
+  "hideSymbolLogo": false,
+  "scalePosition": "right",
+  "scaleMode": "Normal",
+  "fontFamily": "-apple-system, BlinkMacSystemFont, Trebuchet MS, Roboto, Ubuntu, sans-serif",
+  "fontSize": "10",
+  "noTimeScale": false,
+  "valuesTracking": "1",
+  "changeMode": "price-and-percent",
+  "chartType": "area",
+  "maLineColor": "#2962FF",
+  "maLineWidth": 1,
+  "maLength": 9,
+  "gridLineColor": "rgba(240, 243, 250, 0.2)",
+  "backgroundColor": "rgba(0, 0, 0, 1)",
+  "widgetFontColor": "rgba(255, 255, 255, 1)",
+  "lineWidth": 2,
+  "lineType": 0,
+  "dateRanges": [
+    "1d|1",
+    "1m|30",
+    "3m|60",
+    "12m|1D",
+    "60m|1W",
+    "ytd|1D"
+  ]
+}
+  </script>
+</div>
+<!-- TradingView Widget END -->
 };
 
 // Now you can use the tradingViewConfig variable as needed
