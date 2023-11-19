34349764a8a8ab65e84adfc4681647045166a11d
ahntrv <151053073+ahntrv@users.noreply.github.com>
2023-11-18 22:51:10 -0600

    Update script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// script.js
+
+// Wrap the configuration in a variable
+var tradingViewConfig = {
+    "symbols": [
+        ["Apple", "AAPL|1D"],
+        ["Microsoft", "MSFT|1D"],
+        ["NASDAQ:TSLA|1D"],
+        ["NASDAQ:NVDA|1D"]
+    ],
+    "chartOnly": false,
+    "width": "100%", /* Set the widget width to 100% */
+    // ... (rest of the configuration)
+};
+
+// Now you can use the tradingViewConfig variable as needed
